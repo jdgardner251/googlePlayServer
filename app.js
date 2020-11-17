35f2aa188ee7657f0ada5ba2bdc9f8eea1751754
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-11-17 12:14:22 -0600

    working on supertest

--- app.js
+++ app.js
@@ -1,53 +1,52 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan('common'));
+app.use(morgan("common"));
 
-const apps = require('./playstore')
+const apps = require("./playstore");
 
-app.get('/apps', (req, res) => {
-    // code goes here
-    const { sort, genre = "" } = req.query;
-    const validGenres = [ `Action`, `Puzzle`, `Strategy`, `Casual`, `Arcade`, `Card` ];
+app.get("/apps", (req, res) => {
+  // code goes here
+  const { sort, genre = "" } = req.query;
+  const validGenres = [
+    `Action`,
+    `Puzzle`,
+    `Strategy`,
+    `Casual`,
+    `Arcade`,
+    `Card`,
+  ];
 
-    if (sort) {
-        if (!['rating', 'app'].includes(sort)) {
-            return res
-                .status(400)
-                .send('Sor must be one of rating or app')
-        }
+  if (sort) {
+    if (!["rating", "app"].includes(sort)) {
+      return res.status(400).send("Sor must be one of rating or app");
     }
+  }
 
-    if (genre) {
-        if(!(validGenres.includes(genre))) {
-            return res
-                .status(400)
-                .send('Genre must be Action, Puzzle, Strategy, Casual, Arcade, or Card')
-        }
+  if (genre) {
+    if (!validGenres.includes(genre)) {
+      return res
+        .status(400)
+        .send(
+          "Genre must be Action, Puzzle, Strategy, Casual, Arcade, or Card"
+        );
     }
+  }
 
-    let results = apps
-        .filter(app => 
-                app
-                    .Genres
-                    .toLocaleLowerCase()
-                    .includes(genre.toLocaleLowerCase()))
-    
+  let results = apps.filter((app) =>
+    app.Genres.toLocaleLowerCase().includes(genre.toLocaleLowerCase())
+  );
 
-    if (sort) {
-        results
-            .sort((a, b) => {
-                return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-            })
-    }
-    res
-        .json(apps)
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+  res.json(apps);
 });
 
 module.exports = app;
 
-// app.listen(8000, () => {
-//     console.log('server listening on 8000')
-// })
+
--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,25 @@
 const { expect } = require('chai');
 const supertest = require('supertest');
 const app = require('../app');
+
+
+describe('Express App', () => {
+    it('should return a message from GET /', () => {
+      return supertest(app)
+        .get('/')
+        .expect(200, 'Hello Express!');
+    });
+  });
+
+
+describe('app', () => {
+    // define test specs here
+
+    it('spec here', () => {
+        // spec code here
+    })
+
+    it('spec here', () => {
+        // spec code here
+    })
+})
